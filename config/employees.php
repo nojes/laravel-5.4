e7214a40d029f46475a3bf03bd2cfc0c41396317
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2018-04-06 01:00:13 +0300

    Installed "laravel-employees", updated composer.lock

---
+++ config/employees.php
@@ -0,0 +1,98 @@
+<?php
+
+use \nojes\employees\database\seeds\EmployeePositionTableSeeder;
+
+return [
+    // automatic loading of routes through main service provider
+    'routes' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Config for views, rendering.
+    |--------------------------------------------------------------------------
+    */
+    'views' => [
+        /*
+        | The path where the package views are stored.
+        | ('{package-path}/resources/views/' by default)
+        */
+//        'path' => 'resources/views/vendor/employees/'
+
+        'tree' => [
+            'lazyChildren' => true,
+        ]
+    ],
+
+    'publish' => [
+        'views' => false,
+        'migrations' => false,
+        'seeds' => false,
+        'factories' => false,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Config for seeders.
+    |--------------------------------------------------------------------------
+    */
+    'seeds' => [
+        'employee' => [
+            'count' => 1000,
+            'depth' => 5,
+            'states' => [
+                //'defaultPhoto'
+                //'localPhoto'
+            ]
+        ],
+        'position' => [
+            'once' => true,
+            'method' => EmployeePositionTableSeeder::INSERT_METHOD,
+            'titles' => [
+                'Accessibility Specialist',
+                'Agile Project Manager',
+                'Business Systems Analyst',
+                'Cloud Architect',
+                'Computer Graphics Animator',
+                'Computer Network Architect',
+                'Computer Support Specialist',
+                'Content Manager',
+                'Content Strategist',
+                'Data Analyst',
+                'Data Architect',
+                'Data Modeler',
+                'Data Scientist',
+                'Database Administrator',
+                'Database Administrator',
+                'Designer',
+                'DevOps Manager',
+                'Digital Marketing Manager',
+                'Frameworks Specialist',
+                'Front-End Designer',
+                'Front-End Developer',
+                'Full-Stack Developer',
+                'Game Developer',
+                'Growth Hacker',
+                'Information Architect',
+                'Information Security Analyst',
+                'Interaction Designer',
+                'Marketing Technologist',
+                'Mobile App Developer',
+                'Mobile Developer',
+                'Product Manager',
+                'Project Lead',
+                'QA (Quality Assurance) Specialist',
+                'Security Specialist',
+                'SEO Consultant',
+                'Social Media Manager',
+                'Software Developer',
+                'Systems Administrator',
+                'Systems Engineer',
+                'Technical Account Manager',
+                'Technical Lead',
+                'UI Designer',
+                'UX Designer',
+                'Web Analytics Developer',
+            ]
+        ]
+    ]
+];
